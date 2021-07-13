f3f9d5a36ee134257d49ef2dede2c7548db88864
iamvanshj <85945360+iamvanshj@users.noreply.github.com>
2021-07-13 20:59:26 +0530

    quiz for friends

--- About-me.js
+++ About-me.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+var readlineSync = require ("readline-Sync");
+
+var score = 0;
+var userName = readlineSync.question("whats's your name? ")
+
+console.log("welcome "+ userName);
+
+function play(question, answer) {
+ var userAnswer = readlineSync.question(question);
+
+ if (userAnswer === answer) {
+        console.log("You were right!")
+        score = score + 1;
+ }  else{
+        console.log("you were wrong!")
+        score = score - 1;
+ }
+}
+
+//calling the function//
+play("Where do i work? ", "Microsoft");
+
+play("Where do i live? ", "Agra");
+
+console.log("your score is ", score)
